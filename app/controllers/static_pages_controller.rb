63c0653b50e4acac326519478c97bf16dbb5645b
NgoVanDuy <ngo.van.duy@framgia.com>
2017-05-19 13:16:49 +0700

    View film and detail

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
+  before_action :load_film, only: [:show]
+
   def home
+    @search = Film.ransack params[:q]
+    @films = @search.result
+  end
+
+  def show
+  end
+
+  def load_film
+    begin
+      @film = Film.find params[:id]
+    rescue Exception
+      response_to_message t("manage_films.not_found"), root_url
+    end
   end
 end
